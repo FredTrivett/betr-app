551393b5b1332490c53305cc19226b9de60ac63d
Fredeau123 <frederictrivett@gmail.com>
2024-11-01 12:03:09 +0100

    Initial Commit

---
+++ betr/betrApp.swift
@@ -0,0 +1,17 @@
+//
+//  betrApp.swift
+//  betr
+//
+//  Created by Fred Trivett on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct betrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ betrTests/betrTests.swift
@@ -0,0 +1,17 @@
+//
+//  betrTests.swift
+//  betrTests
+//
+//  Created by Fred Trivett on 01/11/2024.
+//
+
+import Testing
+@testable import betr
+
+struct betrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
